97ba532e781caf369d4b01683d356f540e70a178
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-02-14 20:50:39 -0300

    implement insight weather data deserializer

---
+++ src/main/java/com/nasa/api/deserialization/InSightWeatherDataDeserializer.java
@@ -0,0 +1,50 @@
+package com.nasa.api.deserialization;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import com.nasa.api.model.SolData;
+import com.nasa.api.model.InSightWeatherData;
+import com.nasa.api.model.validity.ValidityChecks;
+
+public class InSightWeatherDataDeserializer implements JsonDeserializer<InSightWeatherData> {
+
+    @Override
+    public InSightWeatherData deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+        JsonObject object = json.getAsJsonObject();
+
+        ArrayList<Integer> solKeys = new ArrayList<>();
+        for (JsonElement element : object.get("sol_keys").getAsJsonArray()) {
+            solKeys.add(element.getAsInt());
+        }
+
+        ArrayList<ValidityChecks> validityChecks = new ArrayList<>();
+        for (JsonElement element : object.get("validity_checks").getAsJsonArray()) {
+            validityChecks.add(Deserializer.gson.fromJson(element, ValidityChecks.class));
+        }
+
+        object.remove("sol_keys");
+        object.remove("validity_checks");
+
+        Map<Integer, SolData> solData = new HashMap<>();
+        for (Map.Entry<String, JsonElement> entry : object.entrySet()) {
+            int key = Integer.parseInt(entry.getKey());
+            JsonObject value = entry.getValue().getAsJsonObject();
+            value.addProperty("key", key);
+
+            solData.put(key, Deserializer.gson.fromJson(value, SolData.class));
+        }
+
+        InSightWeatherData inSightWeatherData = new InSightWeatherData(solData, solKeys, validityChecks);
+        return inSightWeatherData;
+    }
+
+}
