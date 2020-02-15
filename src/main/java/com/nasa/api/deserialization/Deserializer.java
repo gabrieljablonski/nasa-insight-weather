01b940735534121754635770fb57cf8baa1f5fac
Gabriel Jablonski <gabriel.jablonski.ufu@gmail.com>
2020-02-14 21:24:33 -0300

    register insight weather data deserializer

--- src/main/java/com/nasa/api/deserialization/Deserializer.java
+++ src/main/java/com/nasa/api/deserialization/Deserializer.java
@@ -2,7 +2,7 @@ package com.nasa.api.deserialization;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-
+import com.nasa.api.model.InSightWeatherData;
 import com.nasa.api.model.SensorData;
 import com.nasa.api.model.compass.CompassData;
 import com.nasa.api.model.compass.CompassDataPoint;
@@ -16,7 +16,6 @@ import com.nasa.api.deserialization.compass.CompassDataPointDeserializer;
 import com.nasa.api.deserialization.validity.CheckDeserializer;
 import com.nasa.api.deserialization.validity.ValidityCheckDeserializer;
 import com.nasa.api.deserialization.validity.ValidityChecksDeserializer;
-import com.nasa.api.deserialization.SolDataDeserializer;
 
 public class Deserializer {
 
@@ -32,6 +31,7 @@ public class Deserializer {
         builder.registerTypeAdapter(ValidityCheck.class, new ValidityCheckDeserializer());
         builder.registerTypeAdapter(ValidityChecks.class, new ValidityChecksDeserializer());
         builder.registerTypeAdapter(SolData.class, new SolDataDeserializer());
+        builder.registerTypeAdapter(InSightWeatherData.class, new InSightWeatherDataDeserializer());
 
 
         gson = builder.create();
